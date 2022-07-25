2675ea517d35d2fc5388eb6e6d3e331e65b82fea
Joanna Świderska <joannaswiderska@MacBook-Pro-Joanna.local>
2022-07-25 22:51:57 +0200

    correct show sum and balance

--- src/index.js
+++ src/index.js
@@ -4,31 +4,27 @@ const createElement = (element) => document.createElement(element);
 let transactionIncome = [];
 let transactionExpenses = [];
 
-const incomeId = querySelector("#incomeId"); // ul income
-const expensesId = querySelector("#expensesId"); // ul expenses
-const categoryIncome = querySelector(".incomeValue"); //option
-const categoryExpenses = querySelector(".expensesValue"); //option
+const incomeId = querySelector("#incomeId");
+const expensesId = querySelector("#expensesId");
+const categoryIncome = querySelector(".incomeValue");
+const categoryExpenses = querySelector(".expensesValue");
 
-const incomeSection = querySelector(".income-area"); //div
-const expensesSection = querySelector(".expenses-area"); // div
-const availableMoney = querySelector(".available-money"); // p
+const incomeSection = querySelector(".income-area");
+const expensesSection = querySelector(".expenses-area");
+const availableMoney = querySelector(".available-money");
 const title = document.getElementById("title");
 const addTransactionPanel = querySelector(".add-transaction-panel");
 
-const nameInput = querySelector("#name"); //input -> add name transaction
-const amountInput = querySelector("#amount"); // input -> add value transaction
-const categorySelect = querySelector("#category"); //select
+const nameInput = querySelector("#name");
+const amountInput = querySelector("#amount");
+const categorySelect = querySelector("#category");
 
 const addTransactionBtn = querySelector(".add-transaction");
 const saveBtn = querySelector(".save");
 const cancelBtn = querySelector(".cancel");
 const deleteAllBtn = querySelector(".delete-all");
 
-// let entryList = [];
-
 let id = 0;
-// let selectedCategory;
-// let moneyArr = [0];
 
 const deleteButtonFunc = (id) => {
   const type = transactionIncome.find(({ id: incomeId }) => incomeId === id)
@@ -116,9 +112,17 @@ const addTransaction = (event) => {
   const name = nameInput.value;
   const value = amountInput.value;
 
-  categorySelect.value === "income"
-    ? (transactionIncome = [...transactionIncome, { name, value, id }])
-    : (transactionExpenses = [...transactionExpenses, { name, value, id }]);
+  if (value == "") {
+    alert("Wpisz poprawną kwotę!");
+  } else if (name == "") {
+    alert("Wpisz poprawną nazwę transakcji!");
+  } else if (value < 1) {
+    alert("Wpisz poprawną kwotę!");
+  } else {
+    categorySelect.value === "income"
+      ? (transactionIncome = [...transactionIncome, { name, value, id }])
+      : (transactionExpenses = [...transactionExpenses, { name, value, id }]);
+  }
 
   sumFunction(categorySelect.value);
 
@@ -175,7 +179,7 @@ const showSum = (type, sumValueIncome, sumValueExpense) => {
 };
 
 const showBudget = (transactionIncome, transactionExpenses) => {
-  console.log(transactionIncome, transactionExpenses);
+  // console.log(transactionIncome, transactionExpenses);
   const sumPanelIncme = transactionIncome
     .map((item) => parseFloat(item.value))
     .reduce((prev, curr) => prev + curr, 0);
@@ -190,16 +194,13 @@ const showBudget = (transactionIncome, transactionExpenses) => {
 
 const showBalance = (sumPanel) => {
   if (sumPanel > 0) {
-    title.innerHTML = "Możesz jeszcze wydać:";
-    console.log(title);
+    title.textContent = "Możesz jeszcze wydać:";
     availableMoney.textContent = `${sumPanel} zł`;
   } else if (sumPanel < 0) {
-    title.innerHTML = "Bilans jest ujemny. Jesteś na minusie:";
-    console.log(title);
+    title.textContent = "Bilans jest ujemny. Jesteś na minusie:";
     availableMoney.textContent = `${sumPanel} zł`;
   } else {
-    title.innerHTML = "Bilans wynosi:";
-    console.log(title);
+    title.textContent = "Bilans wynosi:";
     availableMoney.textContent = `${sumPanel} zł`;
   }
 };
